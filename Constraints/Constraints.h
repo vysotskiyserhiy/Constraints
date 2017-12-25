06216273ac60bb9fc7e5ab6f26276b9e21eaeb12
Serhiy Vysotskiy <serhiy.vysotskiy@appunite.com>
2017-12-25 11:14:33 +0200

    Initial Commit

---
+++ Constraints/Constraints.h
@@ -0,0 +1,19 @@
+//
+//  Constraints.h
+//  Constraints
+//
+//  Created by Serhiy Vysotskiy on 12/25/17.
+//  Copyright © 2017 vysotskiyserhiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Constraints.
+FOUNDATION_EXPORT double ConstraintsVersionNumber;
+
+//! Project version string for Constraints.
+FOUNDATION_EXPORT const unsigned char ConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constraints/PublicHeader.h>
+
+
