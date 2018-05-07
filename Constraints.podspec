3dd1c819a2164dbf7dd95d5d8256cec1776b6fe5
Сергей Высоцкий <vysotskiyserhiy@gmail.com>
2018-05-07 17:24:01 +0300

    iOS 10 compatible

--- Constraints.podspec
+++ Constraints.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Constraints'
-  s.version          = '1.2.3'
+  s.version          = '1.2.4'
   s.summary          = 'Easy Swift programmatic constraints'
   s.homepage         = 'https://github.com/vysotskiyserhiy/Constraints'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
--- Constraints/Classes/Constraint+NSLayoutAnchor.swift
+++ Constraints/Classes/Constraint+NSLayoutAnchor.swift
@@ -25,25 +25,70 @@ extension Constraint {
     }
 }
 
-@available(iOS 11.0, *)
+//@available(iOS 11.0, *)
+//extension Constraint {
+//    public func safePin(_ edges: Edge..., c: CGFloat = 0) -> Constraint {
+//        var newEdges = edges
+//
+//        if edges.isEmpty {
+//            newEdges = [.left, .top, .right, .bottom]
+//        }
+//
+//        return newEdges.set.reduce(self) {
+//            switch $1 {
+//            case .left:
+//                return $0.xAnchor(view.leftAnchor, to: superview.safeAreaLayoutGuide.leftAnchor, c: c)
+//            case .top:
+//                return $0.yAnchor(view.topAnchor, to: superview.safeAreaLayoutGuide.topAnchor, c: c)
+//            case .right:
+//                return $0.xAnchor(view.rightAnchor, to: superview.safeAreaLayoutGuide.rightAnchor, c: -c)
+//            case .bottom:
+//                return $0.yAnchor(view.bottomAnchor, to: superview.safeAreaLayoutGuide.bottomAnchor, c: -c)
+//            }
+//        }
+//    }
+//}
+
 extension Constraint {
     public func safePin(_ edges: Edge..., c: CGFloat = 0) -> Constraint {
-        var newEdges = edges
-        
-        if edges.isEmpty {
-            newEdges = [.left, .top, .right, .bottom]
-        }
-        
-        return newEdges.set.reduce(self) {
-            switch $1 {
-            case .left:
-                return $0.xAnchor(view.leftAnchor, to: superview.safeAreaLayoutGuide.leftAnchor, c: c)
-            case .top:
-                return $0.yAnchor(view.topAnchor, to: superview.safeAreaLayoutGuide.topAnchor, c: c)
-            case .right:
-                return $0.xAnchor(view.rightAnchor, to: superview.safeAreaLayoutGuide.rightAnchor, c: -c)
-            case .bottom:
-                return $0.yAnchor(view.bottomAnchor, to: superview.safeAreaLayoutGuide.bottomAnchor, c: -c)
+        if #available(iOS 11.0, *) {
+            var newEdges = edges
+            
+            if edges.isEmpty {
+                newEdges = [.left, .top, .right, .bottom]
+            }
+            
+            return newEdges.set.reduce(self) {
+                switch $1 {
+                case .left:
+                    return $0.xAnchor(view.leftAnchor, to: superview.safeAreaLayoutGuide.leftAnchor, c: c)
+                case .top:
+                    return $0.yAnchor(view.topAnchor, to: superview.safeAreaLayoutGuide.topAnchor, c: c)
+                case .right:
+                    return $0.xAnchor(view.rightAnchor, to: superview.safeAreaLayoutGuide.rightAnchor, c: -c)
+                case .bottom:
+                    return $0.yAnchor(view.bottomAnchor, to: superview.safeAreaLayoutGuide.bottomAnchor, c: -c)
+                }
+            }
+        } else {
+            guard !edges.isEmpty else {
+                return pin(.left, to: .left, of: view, c: c)
+                    .pin(.top, to: .top, of: view, c: c)
+                    .pin(.right, to: .right, of: view, c: -c)
+                    .pin(.bottom, to: .bottom, of: view, c: -c)
+            }
+            
+            return edges.set.reduce(self) { constraint, edge in
+                switch edge {
+                case .left:
+                    return constraint.pin(.left, to: .left, of: view, c: c)
+                case .top:
+                    return constraint.pin(.top, to: .top, of: view, c: c)
+                case .right:
+                    return constraint.pin(.right, to: .right, of: view, c: -c)
+                case .bottom:
+                    return constraint.pin(.bottom, to: .bottom, of: view, c: -c)
+                }
             }
         }
     }
